5c7a70d164d2892d99d728c1ca0c72b2375c470c
James Richardson <james@time4tea.net>
2012-02-06 08:55:32 +0000

    make more things describable

--- src/java/net/time4tea/StaticMethodRemover.java
+++ src/java/net/time4tea/StaticMethodRemover.java
@@ -51,8 +51,8 @@ public class StaticMethodRemover {
     private static abstract class DescribableDelayed implements Delayed {
         private final String desc;
 
-        protected DescribableDelayed(String desc) {
-            this.desc = desc;
+        protected DescribableDelayed(Object... descripton) {
+            this.desc = Arrays.toString(descripton);
         }
 
         @Override
@@ -84,7 +84,7 @@ public class StaticMethodRemover {
 
             @Override
             public void visitAttribute(final Attribute attr) {
-                delayed.add(new DescribableDelayed("visit attribute " + attr) {
+                delayed.add(new DescribableDelayed("visit attribute",attr) {
                     @Override
                     public void donow() {
                         MyMethodVisitor.super.visitAttribute(attr);
@@ -94,7 +94,7 @@ public class StaticMethodRemover {
 
             @Override
             public void visitCode() {
-                delayed.add(new Delayed() {
+                delayed.add(new DescribableDelayed("code") {
                     @Override
                     public void donow() {
                         MyMethodVisitor.super.visitCode();
@@ -105,7 +105,7 @@ public class StaticMethodRemover {
 
             @Override
             public void visitLabel(final Label label) {
-                delayed.add(new DescribableDelayed("label " + label) {
+                delayed.add(new DescribableDelayed("label",label) {
                     @Override
                     public void donow() {
                         MyMethodVisitor.super.visitLabel(label);
@@ -116,7 +116,7 @@ public class StaticMethodRemover {
 
             @Override
             public void visitFrame(final int type, final int nLocal, final Object[] local, final int nStack, final Object[] stack) {
-                delayed.add(new Delayed() {
+                delayed.add(new DescribableDelayed("frame", type, nLocal, local, nLocal, nStack, stack) {
                     @Override
                     public void donow() {
                         MyMethodVisitor.super.visitFrame(type, nLocal, local, nStack, stack);
@@ -126,7 +126,7 @@ public class StaticMethodRemover {
 
             @Override
             public void visitTryCatchBlock(final Label start, final Label end, final Label handler, final String type) {
-                delayed.add(new Delayed() {
+                delayed.add(new DescribableDelayed("trycatch", start,end, handler,type) {
                     @Override
                     public void donow() {
                         MyMethodVisitor.super.visitTryCatchBlock(start, end, handler, type);
@@ -136,7 +136,7 @@ public class StaticMethodRemover {
 
             @Override
             public void visitLineNumber(final int line, final Label start) {
-                delayed.add(new Delayed() {
+                delayed.add(new DescribableDelayed("linenumber", line, start) {
                     @Override
                     public void donow() {
                         MyMethodVisitor.super.visitLineNumber(line, start);
@@ -146,7 +146,7 @@ public class StaticMethodRemover {
 
             @Override
             public void visitMaxs(final int maxStack, final int maxLocals) {
-                delayed.add(new Delayed() {
+                delayed.add(new DescribableDelayed("maxs", maxStack,maxLocals) {
                     @Override
                     public void donow() {
                         MyMethodVisitor.super.visitMaxs(maxStack, maxLocals);
@@ -157,7 +157,7 @@ public class StaticMethodRemover {
             @Override
             public void visitInsn(final int opcode) {
 
-                delayed.add(new Delayed() {
+                delayed.add(new DescribableDelayed("insn", opcode) {
                     @Override
                     public void donow() {
                         MyMethodVisitor.super.visitInsn(opcode);
@@ -168,7 +168,7 @@ public class StaticMethodRemover {
 
             @Override
             public void visitIntInsn(final int opcode, final int operand) {
-                delayed.add(new Delayed() {
+                delayed.add(new DescribableDelayed("intinsn", opcode, operand) {
                     @Override
                     public void donow() {
                         MyMethodVisitor.super.visitIntInsn(opcode, operand);
@@ -178,7 +178,7 @@ public class StaticMethodRemover {
 
             @Override
             public void visitVarInsn(final int opcode, final int var) {
-                delayed.add(new Delayed() {
+                delayed.add(new DescribableDelayed("varissn", opcode, var) {
                     @Override
                     public void donow() {
                         MyMethodVisitor.super.visitVarInsn(opcode, var);
@@ -188,7 +188,7 @@ public class StaticMethodRemover {
 
             @Override
             public void visitTypeInsn(final int opcode, final String type) {
-                delayed.add(new DescribableDelayed("type insn" + opcode + "/" + type) {
+                delayed.add(new DescribableDelayed("type insn" ,opcode , type) {
                     @Override
                     public void donow() {
                         MyMethodVisitor.super.visitTypeInsn(opcode, type);
@@ -199,7 +199,7 @@ public class StaticMethodRemover {
 
             @Override
             public void visitFieldInsn(final int opcode, final String owner, final String name, final String desc) {
-                delayed.add(new DescribableDelayed("fields insn" + opcode + "/" + owner + "/" + name + "/" + desc) {
+                delayed.add(new DescribableDelayed("fields insn" , opcode , owner , name , desc) {
                     @Override
                     public void donow() {
                         MyMethodVisitor.super.visitFieldInsn(opcode, owner, name, desc);
@@ -212,13 +212,94 @@ public class StaticMethodRemover {
                 if (matcher.matches(new MethodSignature(owner, name, desc))) {
                     delayed.clear();
                 } else {
-                    delayed.add(new DescribableDelayed("method " + owner + "/" + name + "/" + desc) {
+                    delayed.add(new DescribableDelayed("method " , owner ,  name , desc) {
                         @Override
                         public void donow() {
                             MyMethodVisitor.super.visitMethodInsn(opcode, owner, name, desc);
                         }
                     });
                 }
+            }
+
+            @Override
+            public void visitInvokeDynamicInsn(final String name, final String desc, final Handle bsm, final Object... bsmArgs) {
+                delayed.add(new DescribableDelayed("invoke dynamic ", name, desc, bsm, Arrays.toString(bsmArgs)) {
+                    @Override
+                    public void donow() {
+                        MyMethodVisitor.super.visitInvokeDynamicInsn(name, desc, bsm, bsmArgs);
+                    }
+                });
+                replay();
+            }
+
+            @Override
+            public void visitJumpInsn(final int opcode, final Label label) {
+                delayed.add(new DescribableDelayed("jump ", label) {
+                    @Override
+                    public void donow() {
+                        MyMethodVisitor.super.visitJumpInsn(opcode, label);
+                    }
+                });
+            }
+
+            @Override
+            public void visitLdcInsn(final Object cst) {
+                delayed.add(new DescribableDelayed("ldc ", cst) {
+                    @Override
+                    public void donow() {
+                        MyMethodVisitor.super.visitLdcInsn(cst);
+                    }
+                });
+            }
+
+            @Override
+            public void visitIincInsn(final int var, final int increment) {
+                delayed.add(new DescribableDelayed("iinc ", var, increment) {
+                    @Override
+                    public void donow() {
+                        MyMethodVisitor.super.visitIincInsn(var, increment);
+                    }
+                });
+            }
+
+            @Override
+            public void visitTableSwitchInsn(final int min, final int max, final Label dflt, final Label... labels) {
+                delayed.add(new DescribableDelayed("tableswitch", min, max, dflt,labels) {
+                    @Override
+                    public void donow() {
+                        MyMethodVisitor.super.visitTableSwitchInsn(min, max, dflt, labels);
+                    }
+                });
+            }
+
+            @Override
+            public void visitLookupSwitchInsn(final Label dflt, final int[] keys, final Label[] labels) {
+                delayed.add(new DescribableDelayed("lookupswitch", dflt, keys, labels) {
+                    @Override
+                    public void donow() {
+                        MyMethodVisitor.super.visitLookupSwitchInsn(dflt, keys, labels);
+                    }
+                });
+            }
+
+            @Override
+            public void visitMultiANewArrayInsn(final String desc, final int dims) {
+                delayed.add(new DescribableDelayed("multianewarray") {
+                    @Override
+                    public void donow() {
+                        MyMethodVisitor.super.visitMultiANewArrayInsn(desc, dims);
+                    }
+                });
+            }
+
+            @Override
+            public void visitLocalVariable(final String name, final String desc, final String signature, final Label start, final Label end, final int index) {
+                delayed.add(new DescribableDelayed("localvariable") {
+                    @Override
+                    public void donow() {
+                        MyMethodVisitor.super.visitLocalVariable(name, desc, signature, start, end, index);
+                    }
+                });
             }
 
             @Override
@@ -232,87 +313,6 @@ public class StaticMethodRemover {
                     d.donow();
                 }
                 delayed.clear();
-            }
-
-            @Override
-            public void visitInvokeDynamicInsn(final String name, final String desc, final Handle bsm, final Object... bsmArgs) {
-                delayed.add(new DescribableDelayed("invoke dynamic " + name + "/" + desc + "/" + bsm + " / " + Arrays.toString(bsmArgs)) {
-                    @Override
-                    public void donow() {
-                        MyMethodVisitor.super.visitInvokeDynamicInsn(name, desc, bsm, bsmArgs);
-                    }
-                });
-                replay();
-            }
-
-            @Override
-            public void visitJumpInsn(final int opcode, final Label label) {
-                delayed.add(new Delayed() {
-                    @Override
-                    public void donow() {
-                        MyMethodVisitor.super.visitJumpInsn(opcode, label);
-                    }
-                });
-            }
-
-            @Override
-            public void visitLdcInsn(final Object cst) {
-                delayed.add(new Delayed() {
-                    @Override
-                    public void donow() {
-                        MyMethodVisitor.super.visitLdcInsn(cst);
-                    }
-                });
-            }
-
-            @Override
-            public void visitIincInsn(final int var, final int increment) {
-                delayed.add(new Delayed() {
-                    @Override
-                    public void donow() {
-                        MyMethodVisitor.super.visitIincInsn(var, increment);
-                    }
-                });
-            }
-
-            @Override
-            public void visitTableSwitchInsn(final int min, final int max, final Label dflt, final Label... labels) {
-                delayed.add(new Delayed() {
-                    @Override
-                    public void donow() {
-                        MyMethodVisitor.super.visitTableSwitchInsn(min, max, dflt, labels);
-                    }
-                });
-            }
-
-            @Override
-            public void visitLookupSwitchInsn(final Label dflt, final int[] keys, final Label[] labels) {
-                delayed.add(new Delayed() {
-                    @Override
-                    public void donow() {
-                        MyMethodVisitor.super.visitLookupSwitchInsn(dflt, keys, labels);
-                    }
-                });
-            }
-
-            @Override
-            public void visitMultiANewArrayInsn(final String desc, final int dims) {
-                delayed.add(new Delayed() {
-                    @Override
-                    public void donow() {
-                        MyMethodVisitor.super.visitMultiANewArrayInsn(desc, dims);
-                    }
-                });
-            }
-
-            @Override
-            public void visitLocalVariable(final String name, final String desc, final String signature, final Label start, final Label end, final int index) {
-                delayed.add(new Delayed() {
-                    @Override
-                    public void donow() {
-                        MyMethodVisitor.super.visitLocalVariable(name, desc, signature, start, end, index);
-                    }
-                });
             }
         }
 
